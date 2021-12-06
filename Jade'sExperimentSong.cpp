7a308ef548bdc23df5bc989f37a02241aa087cad
Jade Kleckner <83440088+j-kleckner@users.noreply.github.com>
2021-12-06 15:42:50 -0600

    Add files via upload

---
+++ Jade'sExperimentSong.cpp
@@ -0,0 +1,41 @@
+#include <iostream>      //For I/O Functions
+#include <windows.h>   //For Beep & Sleep functions
+#include<stdlib.h>  //For System Pause
+using namespace std;
+
+int main() {
+	Beep (329, 300);Sleep(100);
+	Beep (392, 150);Sleep(100);
+	Beep (329, 450);Sleep(100);
+	Beep (220, 150);Sleep(100);
+	Beep (329, 300);Sleep(100);
+	Beep (329, 150);Sleep(100);
+	Beep (294, 150);Sleep(100);
+	Beep (329, 300);Sleep(100);
+	Beep (262, 300);Sleep(100);
+	Beep (329, 300);Sleep(300);
+	Beep (329, 150);Sleep(100);
+	Beep (392, 150);Sleep(100);
+	Beep (329, 150);Sleep(200);
+	Beep (220, 150);Sleep(100);
+	Beep (262, 150);Sleep(100);
+	Beep (220, 150);Sleep(100);
+	Beep (262, 300);Sleep(300);
+	Beep (392, 150);Sleep(100);
+	Beep (329, 300);Sleep(100);
+	Beep (329, 150);Sleep(100);
+	Beep (444, 300);Sleep(100);
+	Beep (329, 300);Sleep(200);
+	Beep (262, 150);Sleep(100);
+	Beep (220, 150);Sleep(100);
+	Beep (329, 450);Sleep(300);  
+	Beep (329, 300);Sleep(300);
+	Beep (220, 150);Sleep(100);
+	Beep (262, 300);Sleep(100);
+	Beep (220, 150);Sleep(100);
+	Beep (294, 450);Sleep(100);  
+	 
+    system ("pause");
+        
+        return 0;
+    } 
